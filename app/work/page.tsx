deb40d8f187fc3830108a340fbbe83d8930eb883
Jose Benitez <jose@intuitivo.com>
2024-06-07 18:59:28 -0400

    update work page and add new blog

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,10 +1,105 @@
+// export default function Page() {
+//   return (
+//     <section>
+//       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+//       <p className="prose prose-neutral dark:prose-invert">
+//         This is where your work experience goes.
+//       </p>
+//     </section>
+//   );
+// }
+// export default function Page() {
+//   return (
+//     <section className="max-w-4xl mx-auto p-8">
+//       <h1 className="font-medium text-3xl mb-8 tracking-tighter text-center">Background</h1>
+//       <div className="prose prose-neutral dark:prose-invert mb-8">
+//         <p>
+//           As an electrical engineer and entrepreneur, I am passionate about creating new technolgy. I founded Intuitivo, a startup that creates the AI Infraestructure for the unattended retail. I lead the Vision AI Team and oversee the development of the infrastructure for millions of autonomous points of purchase, using deep learning, IoT, and data science as core competencies.
+//         </p>
+//       </div>
+//       <div className="prose prose-neutral dark:prose-invert mb-8">
+//         <h2 className="font-semibold text-xl mb-4">Experience</h2>
+//         <ul>
+//           <li>Founder & Director of AI at Intuitivo (2019 - Present)</li>
+//           <li>Founder & CTO at Aratiri (2017 - 2019)</li>
+//           <li>Research & Development at Po Paraguay (2016 - 2017)</li>
+//           <li>Project Developer at Función Digital (2016 - 2017)</li>
+//         </ul>
+//       </div>
+//       <div className="prose prose-neutral dark:prose-invert mb-8">
+//         <h2 className="font-semibold text-xl mb-4">Skills</h2>
+//         <ul>
+//           <li>Multi-Task Learning</li>
+//           <li>Transfer Learning</li>
+//           <li>Meta-Learning Algorithms</li>
+//           <li>Deep Learning</li>
+//           <li>Computer Vision</li>
+//         </ul>
+//       </div>
+//       <div className="prose prose-neutral dark:prose-invert mb-8">
+//         <h2 className="font-semibold text-xl mb-4">Languages</h2>
+//         <ul>
+//           <li>Spanish (Full Professional)</li>
+//           <li>English (Full Professional)</li>
+//           <li>Portuguese (Professional Working)</li>
+//         </ul>
+//       </div>
+//       <div className="prose prose-neutral dark:prose-invert mb-8">
+//         <h2 className="font-semibold text-xl mb-4">Honors and Awards</h2>
+//         <ul>
+//           <li>MIT Most Innovator Under 35</li>
+//           <li>O1 Visa Holder</li>
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// }
+
 export default function Page() {
   return (
-    <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
+    <section className="max-w-4xl mx-auto p-8">
+      <h1 className="font-medium text-3xl mb-8 tracking-tighter text-center">About</h1>
+      <p className="prose prose-neutral dark:prose-invert mb-8">
+        I’m Jose Benitez Genes, an electrical engineer and entrepreneur passionate about creating new tech. Originally from Asunción, Paraguay, I am the Founder and Director of AI at Intuitivo, a startup that creates the AI Infraestructure for the unattended retail
       </p>
+      <p className="prose prose-neutral dark:prose-invert mb-8">
+        I lead the AI team and oversee the development of the infrastructure for millions of autonomous points of purchase, using deep learning, IoT, and data science as core competencies. My previous roles have included founding and serving as CTO of Aratiri, a startup focused on digital manufacturing, 3D design, 3D printing and IoT development.
+      </p>
+      <p className="prose prose-neutral dark:prose-invert mb-8">
+        My journey began with a deep passion for technology and a drive to solve complex problems. As an advocate for innovation, I have consistently aimed to create solutions that make a significant impact. This includes my work at NutriHuevos, Po Paraguay, Función Digital, and Max Maquinas S.A.
+      </p>
+      <h2 className="font-semibold text-2xl mb-4">What I've beed doing</h2>
+      <ul className="list-disc pl-6 prose prose-neutral dark:prose-invert mb-8">
+        <li>Founded Intuitivo, pioneering the development of autonomous points of purchase with cutting-edge AI and IoT technologies.</li>
+        <li>Contributed to the development of digital manufacturing and IoT solutions at Aratiri. (the first one in its class in Paraguay)</li>
+        <li>Developed research and production strategies at Po Paraguay, enhancing the quality of 3D printed prostheses.</li>
+        <li>Worked on smart building technologies at Función Digital.</li>
+      </ul>
+      {/* <h2 className="font-semibold text-2xl mb-4">Skills</h2>
+      <ul className="list-disc pl-6 prose prose-neutral dark:prose-invert mb-8">
+        <li>Multi-Task Learning</li>
+        <li>Transfer Learning</li>
+        <li>Meta-Learning Algorithms</li>
+      </ul>
+      <h2 className="font-semibold text-2xl mb-4">Languages</h2>
+      <ul className="list-disc pl-6 prose prose-neutral dark:prose-invert mb-8">
+        <li>Spanish (Full Professional)</li>
+        <li>Guarani (Full Professional)</li>
+        <li>English (Full Professional)</li>
+        <li>Portuguese (Professional Working)</li>
+      </ul>
+      <h2 className="font-semibold text-2xl mb-4">Certifications</h2>
+      <ul className="list-disc pl-6 prose prose-neutral dark:prose-invert mb-8">
+        <li>COMPUTER VISION I (PYTHON)</li>
+        <li>OpenCV 101 — OpenCV Basics</li>
+        <li>XCS330 - Deep Multi-Task and Meta Learning</li>
+        <li>Deep Learning with PyTorch: Image Segmentation</li>
+      </ul> */}
+      <h2 className="font-semibold text-2xl mb-4">Honors and Awards</h2>
+      <ul className="list-disc pl-6 prose prose-neutral dark:prose-invert mb-8">
+        <li>Innovator Under 35</li>
+        <li>O1 Visa Holder</li>
+      </ul>
     </section>
   );
 }
