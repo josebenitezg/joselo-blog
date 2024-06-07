59d3a389e26db08166f01df82d0466a2419f6f85
Jose Benitez <jose@intuitivo.com>
2024-06-07 12:02:22 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,10 @@
-"use client";
-
-import React from 'react';
-import Tweets from "./components/TwitterFeed";
-
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Hey There, Welcome! 👋🏼</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        I'm Jose.<br />
-        Obsessed with neural networks and biohacking. I tinker with awesome tech at{' '}
-        <a href="https://www.intuitivo.com/" target="_blank" rel="noopener noreferrer">
-          intuitivo.com
-        </a>.
-        Tech geek at heart, open-source lover, and always eager to plug into new machine learning adventures!
+        This is your new portfolio.
       </p>
-      <p className="my-8" />
-      <p className="prose prose-neutral dark:prose-invert">
-        📝{' '}
-        <a
-          href="https://aws.amazon.com/blogs/machine-learning/intuitivo-achieves-higher-throughput-while-saving-on-ai-ml-costs-using-aws-inferentia-and-pytorch/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          AWS Blog Colab - Talking about Inf1 and Inf2 Chips
-        </a>
-      </p>
-      <p className="prose prose-neutral dark:prose-invert">
-        📹{' '}
-        <a
-          href="https://www.youtube.com/live/xWZ9mW7Z4Tc?si=e9eTO6KDl4xBdAHA&t=21551"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          YoloVision Conf - The Power of Foundation Model in Data Collection
-        </a>
-      </p>
-      <p className="prose prose-neutral dark:prose-invert">
-        📹{' '}
-        <a
-          href="https://www.youtube.com/watch?v=IXhUOPBqyz4"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Funny talk with Satya Mallick, CEO at OpenCV, the world's largest computer vision library
-        </a>
-      </p>
-      <Tweets />
     </section>
   );
 }
-
